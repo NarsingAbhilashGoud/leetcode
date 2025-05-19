aa780af9a43e39f1ee86765eb1c5843a8bb831cf
Narsing Abhilash Goud <165760529+NarsingAbhilashGoud@users.noreply.github.com>
2025-05-19 09:29:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (59.47%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -1,21 +1,19 @@
 class Solution {
     static void reverse(int i, int[] arr, int n){
-        if(i==n/2) return;
+        while(i<=n) {
         int temp = arr[i];
-        arr[i] = arr[n-i-1];
-        arr[n-i-1] = temp;
-        reverse(i+1,arr,n); 
-    }
-    public int[][] flipAndInvertImage(int[][] image) {
+        arr[i] = arr[n];
+        arr[n] = temp; 
+        i++;
+        n--;
+        }
+    } public int[][] flipAndInvertImage(int[][] image) {
         int n = image.length, m = image[0].length;
         for(int i=0;i<n;i++){
             int k = image[i].length;
-            reverse(0,image[i],k);
+            reverse(0,image[i],k-1);
             for(int j=0;j<m;j++){
                 if(image[i][j] == 1) image[i][j] =0;
-                else image[i][j] =1;
-            
-            }
-        }return image;
-    }
-}
+                else image[i][j] =1;  
+            }}return image;
+    }}
